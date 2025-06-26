751cf53900340b23cfc4361403f951afcc9af501
Kentillation <kentanthony070495@gmail.com>
2025-06-26 22:34:30 +0800

    June 26, 2025

--- src/components/Snackbar.vue
+++ src/components/Snackbar.vue
@@ -2,10 +2,10 @@
 <template>
   <v-container>
     <v-snackbar v-model="snackbar.visible" :color="snackbar.color" location="bottom" timeout="5000">
-        <h3 class="text-center px-2">
+        <h4 class="text-center px-2">
           <span><v-icon icon="mdi-bell-outline"></v-icon></span>
           <span>&nbsp; {{ snackbar.message }}</span>
-        </h3>
+        </h4>
     </v-snackbar></v-container>
 </template>
 <script>
